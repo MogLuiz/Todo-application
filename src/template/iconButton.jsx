951a639fc1b0063e79a633f939e774c73c9d2af4
Luiz Henrique <luizhjramos@outlook.com>
2021-09-05 20:14:09 -0300

    feat: If component

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,15 +1,15 @@
 
+// Packages
 import React from 'react'
 
-export default props => {
-    if(props.hide) {
-        return null
-    } else {
-      return (
-        <button className={"btn btn-"+ props.style}
-                onClick={props.onClick}>
-            <i className={"fa fa-"+ props.icon}/>
-        </button>
-      )  
-    }
-}
+// Components
+import If from './if'
+
+export default props => (
+  <If test={!props.hide}>
+      <button className={"btn btn-"+ props.style}
+         onClick={props.onClick}>
+         <i className={"fa fa-"+ props.icon}/>
+      </button>
+  </If>
+)
