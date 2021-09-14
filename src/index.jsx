eaf53c583875af39c9ecb7ee8cc7a63bda59d2e0
Luiz Henrique <luizhjramos@outlook.com>
2021-09-14 20:02:10 -0300

    feat: connect application with redux devtools

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,9 @@ import { Provider } from 'react-redux'
 
 import rootReducer from './main/reducers'
 
-const store = createStore(rootReducer)
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(rootReducer, devTools)
 
 ReactDOM.render(
         <Provider store={store}>
