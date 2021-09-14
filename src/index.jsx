a7056149e97abc422efb56844d06d16f7528b19b
Luiz Henrique <luizhjramos@outlook.com>
2021-09-13 22:37:06 -0300

    feat: criando Provider do Redux e integrando com a aplicação

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// Redux
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './main/reducers'
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    , document.getElementById('app'))
