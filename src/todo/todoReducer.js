99f181c8c17f18c92d196c45f7ebd83d3d96a76d
Luiz Henrique <luizhjramos@outlook.com>
2021-09-16 19:39:08 -0300

    feat: cleaning initial state

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -1,24 +1,14 @@
 const INITIAL_STATE = {  
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura do cartão',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Daily às 14:30',
-        done: false,
-    }, {
-        _id: 3,
-        description: 'Terminas meus cursos e começar os outros',
-        done: false
-    }]  
+    description: '',
+    list: []  
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data }
         default:
             return state
     }
