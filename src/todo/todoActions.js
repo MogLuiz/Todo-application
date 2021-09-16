2473b7b84c52e3e583d9903bcbd4a84f5e66b5e9
Luiz Henrique <luizhjramos@outlook.com>
2021-09-16 18:54:48 -0300

    feat: action creator Search

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -7,3 +7,10 @@ export const changeDescription = event => ({
     payload: event.target.value
 })
 
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
